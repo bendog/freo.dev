139c2878b274af70bb3a4cc7e164105938f971a6
Ben Fitzhardinge <ben@fitzhardinge.net>
2019-04-23 15:20:41 +0800

    testing new deployment method

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ./build.sh
-aws s3 sync ./ s3://freo.pub/ --exclude="*.git/*" --exclude="*src/*" --exclude="*.sh" --exclude="*.DS_Store*"
+aws s3 sync ./ s3://freo.pub/ --delete --exclude="*.git/*" --exclude="*src/*" --exclude="*.sh" --exclude="*.DS_Store*"
